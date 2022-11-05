7209d6c169d532105407595d658b9a3ddbfd4b7b
Marcel Schwarz <mschwarz126@gmail.com>
2022-11-05 23:17:25 +0100

    Write comment //47

--- MyRandomPhoto/ViewController.swift
+++ MyRandomPhoto/ViewController.swift
@@ -63,7 +63,7 @@ class ViewController: UIViewController {
         )
     }
 
-    func getRandomPhoto() { //4
+    func getRandomPhoto() { //47
         let urlString = "https://source.unsplash.com/random/600x600"
         let url = URL(string: urlString)!
         guard let data = try? Data(contentsOf: url) else {
